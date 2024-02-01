72faef2062ccc771b19d7a90cd4a56440b581400
luissiito <luis.redes.udla@gmail.com>
2024-01-31 23:00:45 -0300

    He agregado el componente Modal de bootstrap

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('hola mundo')
+
+iniciarBootStrapTooltip();
+
+function iniciarBootStrapTooltip() {
+    $(function () {
+        let btnSubmit = $('#btnSubmit[data-toggle="modal"]');
+        btnSubmit.addEventListener('click', (e)=>{
+            e.preventDefault();
+            btnSubmit.modal()
+        });
+    })   
+}
+
+const linkGithub = document.getElementById('linkGithub');
+const linkLinkedin = document.getElementById('linkLinkedin');
+const linkTwitter = document.getElementById('linkTwitter');
+const linkFacebook = document.getElementById('linkFacebook');
+
+const links = [linkGithub, linkLinkedin, linkTwitter, linkFacebook]
+
+for(let l of links){
+    l.addEventListener('mouseover', function(){
+        l.children[1].classList.toggle('footerToolTip')
+    });
+    l.addEventListener('mouseout', function(){
+        l.children[1].classList.toggle('footerToolTip')
+    });
+}
+
+const btnEnviarModalFooter = document.querySelector('.modal-footer .btn-primary');
+btnEnviarModalFooter.addEventListener('click', () =>{
+    alert('Hubo problemas al intentar comunicarse con el servidor. Tus datos no fueron guardados.')
+    location.reload();
+});
