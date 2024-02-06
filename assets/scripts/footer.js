f343677897e5af47b37a67d1a527cf47ba998947
luissiito <luis.redes.udla@gmail.com>
2024-02-06 15:49:19 -0300

    Cambio de dirección de enlace al logoViajesChile

--- assets/scripts/footer.js
+++ assets/scripts/footer.js
@@ -2,6 +2,6 @@
 añadirEventoClickAlLogoDelFoot();
 function añadirEventoClickAlLogoDelFoot(){
     document.querySelector('footer h3').addEventListener('click', () =>{
-        window.location.href = 'http://127.0.0.1:5500/PruebaFinalModulo2ViajesChile';
+        window.location.href = 'https://luissiito.github.io/PruebaFinalModulo2ViajesChile/';
     });
 }
--- assets/scripts/navBar.js
+++ assets/scripts/navBar.js
@@ -3,11 +3,11 @@ añadirEventoClickAlLogoNavBarSvg();
 añadirEventoClickAlLogoNavBar();
 function añadirEventoClickAlLogoNavBarSvg(){
     document.querySelector('nav .logoViajesChile img').addEventListener('click', () =>{
-        window.location.href = 'http://127.0.0.1:5500/PruebaFinalModulo2ViajesChile';
+        window.location.href = 'https://luissiito.github.io/PruebaFinalModulo2ViajesChile/';
     });
 }
 function añadirEventoClickAlLogoNavBar(){
     document.querySelector('nav div a.navbar-brand').addEventListener('click', () =>{
-        window.location.href = 'http://127.0.0.1:5500/PruebaFinalModulo2ViajesChile';
+        window.location.href = 'https://luissiito.github.io/PruebaFinalModulo2ViajesChile/';
     });
 }
