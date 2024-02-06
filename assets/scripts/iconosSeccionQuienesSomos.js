63f655cc0feac763275c04685e92910f4089bdd5
luissiito <luis.redes.udla@gmail.com>
2024-02-06 01:02:12 -0300

    Agregué un toolpip a cada card de la sección QuienesSomos

--- assets/scripts/iconosSeccionQuienesSomos.js
+++ assets/scripts/iconosSeccionQuienesSomos.js
@@ -1,32 +1,40 @@
 
 function iniciarIconosSeccionQuienesSomos(){
     const iconos = document.querySelectorAll('section div div h2');
-    const alertBootstrap = document.querySelector('#quienesSomos div.alert');
+    const alertBootstrapSeccionUno = document.querySelector('#quienesSomos div div:nth-child(1) .alert');
+    const alertBootstrapSeccionDos = document.querySelector('#quienesSomos div div:nth-child(2) .alert');
+    const alertBootstrapSeccionTres = document.querySelector('#quienesSomos div div:nth-child(3) .alert');
     iconos.forEach((e) =>{
         e.addEventListener('click', ()=>{
             e.classList.add('noDisponible');
             if(e.children[0].classList.contains('fa-plane')){
-                alertBootstrap.classList.add('bg-primary');
-                alertBootstrap.classList.remove('bg-success');
+                alertBootstrapSeccionUno.classList.add('bg-primary');
+                alertBootstrapSeccionUno.classList.remove('bg-success');
+                alertBootstrapSeccionUno.classList.add('disponible');
 
             }
             else if(e.children[0].classList.contains('fa-triangle-exclamation')){
-                alertBootstrap.classList.add('bg-success');
-                alertBootstrap.classList.remove('bg-primary');
+                alertBootstrapSeccionDos.classList.add('bg-success');
+                alertBootstrapSeccionDos.classList.remove('bg-primary');
+                alertBootstrapSeccionDos.classList.add('disponible');
 
             }
             else{
-                alertBootstrap.classList.add('alert-primary');                
-                alertBootstrap.classList.remove('bg-primary');
-                alertBootstrap.classList.remove('bg-success');
+                alertBootstrapSeccionUno.classList.add('alert-primary');                
+                alertBootstrapSeccionUno.classList.remove('bg-primary');
+                alertBootstrapSeccionUno.classList.remove('bg-success');
+                alertBootstrapSeccionTres.classList.add('disponible');
             }
             
-            alertBootstrap.classList.add('disponible');
         });
 
         e.addEventListener('mouseout', ()=>{
-            alertBootstrap.classList.remove('disponible')
-            alertBootstrap.classList.add('noDisponible')
+            alertBootstrapSeccionUno.classList.remove('disponible')
+            alertBootstrapSeccionUno.classList.add('noDisponible')
+            alertBootstrapSeccionDos.classList.remove('disponible')
+            alertBootstrapSeccionDos.classList.add('noDisponible')
+            alertBootstrapSeccionTres.classList.remove('disponible')
+            alertBootstrapSeccionTres.classList.add('noDisponible')
         });
     });
 }
